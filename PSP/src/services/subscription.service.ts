ac41807ff095836b8128d39679791e30aec00297
milicaurukalo <urukalomilica@gmail.com>
2023-01-24 19:49:17 +0100

    Subscribe webShop

--- PSP/src/services/subscription.service.ts
+++ PSP/src/services/subscription.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { SubscriptionDto } from 'src/model/subscription-dto';
+import { AppConstants } from 'src/utils/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  subscribeWebShop(input: SubscriptionDto) {
+    const apiUrl = AppConstants.API_HOST + AppConstants.SUBSCRIPTION.ADD;
+    return this.http.post(apiUrl, input).pipe(map((item: any) => {
+       return item;
+      }))
+  }
 }
